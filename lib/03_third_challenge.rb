f9fb58c7e40fc99fe63976674b1d43922879f6a5
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-05 17:21:56 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -10,7 +10,7 @@ def third_challenge
         age: "54"
         },
       :hero => {
-        name: "Romeo"
+        name: "Romeo",
         age: "15",
         status: "alive"
         },
@@ -18,16 +18,16 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet"
+        name: "Lord Capulet",
         age: "50"
         },
       :matriarch => {
-        name: "Lady Capulet"
+        name: "Lady Capulet",
         age: "51"
         },
       :heroine => {
         name: 'Juliet',
-        age: "15,"
+        age: "15,",
         status: "alive"
         },
       :heroine_friends => []
