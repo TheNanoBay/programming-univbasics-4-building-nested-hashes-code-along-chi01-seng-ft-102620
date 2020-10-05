26a17654b371fc52411def9989c87268bb0d7d4f
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-05 17:20:13 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,16 +2,16 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
+        name: "Lord Montague",
+        age:"53"
         },
       :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
+        name: "Lady Montague",
+        age: "54"
         },
       :hero => {
-        :name => "Romeo"
-        :age => "15",
+        name: "Romeo"
+        age: "15",
         :status => "alive"
         },
       :hero_friends => []
